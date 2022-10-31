c340c16a44401ea3b8b3071c409a548c8e21e2f3
Nathan Monteiro <contato.mont@gmail.com>
2022-10-31 17:09:11 -0300

    Adding media queries for the header part

--- code/script.js
+++ code/script.js
@@ -1,10 +1,22 @@
 'use strict'
 
+//VARIABLES
 const btnOpenMenu = document.querySelector('.hamb-nav');
 const optionsMenu = document.querySelectorAll('.options');
+// const teste = document.querySelector('.container-section-concept');
 
-for (let i=0 ; i<optionsMenu.length ; i++) {
+for (let i = 0; i < optionsMenu.length; i++) {
   btnOpenMenu.addEventListener('click', function () {
     optionsMenu[i].classList.toggle('hidden');
   });
 }
+
+
+//RESIZE ON WINDOWN
+// window.addEventListener('resize', function () {
+//   if (window.innerWidth >= 700) {
+//     teste.style.background = "#62ff01"
+//   } else {
+//     teste.style.background = "black"
+//   }
+// });
